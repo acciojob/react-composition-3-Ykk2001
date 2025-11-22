d7576a317cb66b37abafa6a9c584a55748e98fb7
Ykk2001 <yogeshkhatake18@gmail.com>
2025-11-22 21:28:03 +0530

    Correct

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,27 @@
-
 import React from "react";
-import "../styles/Tooltip.css"
+import "../styles/Tooltip.css";
 import Tooltip from "./Tooltip";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-        
-          
-            <h2 className="tooltip">
-              <span>Hover over me</span>
-              <Tooltip text="This is a tooltip" />
-              </h2>
-        
-           <hr></hr>
-        
-          <Tooltip text="This is another tooltip">
-            <p>Hover over me to see another tooltip</p>
-          </Tooltip>
+      {/* Do not remove the main div */}
 
-          <hr></hr>
-        
+      <h2 className="tooltip">
+        <Tooltip text="This is a tooltip">Hover over me</Tooltip>
+      </h2>
+
+      <hr></hr>
+
+      <p className="tooltip">
+        <Tooltip text="This is another tooltip">
+          Hover over me to see another tooltip
+        </Tooltip>
+      </p>
+
+      <hr></hr>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -3,13 +3,12 @@ import React, { useState } from "react";
 export default function Tooltip({ text, children }) {
   const [visible, setVisible] = useState(false);
   return (
-    <span
+    <div
       onMouseEnter={() => setVisible(true)}
       onMouseLeave={() => setVisible(false)}
     >
       {children}
-      {/* one time it render as h2  */}
       {visible && <div className="tooltiptext">{text}</div>}
-    </span>
+    </div>
   );
 }
